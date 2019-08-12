e4d6980d4f403ae85d2a105e4e9aa090cc3f31e6
Will Murphy <mail@willmurphy.co.uk>
2019-08-12 12:23:19 +0100

    Pushed watchapp source

---
+++ src-watchapp/README.md
@@ -0,0 +1,3 @@
+Monzo for Pebble
+=========
+
---
+++ src-watchapp/src/js/app.js
@@ -0,0 +1,30 @@
+var UI = require('ui');
+var Settings = require('settings');
+
+
+var landingCard = new UI.Card({
+  title: 'Generate Token',
+});
+landingCard.body('Press Select to Generate');
+landingCard.style("small");
+landingCard.show();
+
+landingCard.on('click', function(e) {
+  if (e.button == 'select') {
+    Pebble.getTimelineToken(function(token) {
+      console.log('My timeline token is ' + token);
+      landingCard.body('Token: ' + token);
+      Settings.option("token", token);
+    }, function(error) {
+      console.log('Error getting timeline token: ' + error);
+      landingCard.subtitle('Error: ' + error);
+    });
+  }
+});
+
+Settings.config(
+  { url: "https://willow.systems/pebble-generateToken", hash: true },
+  function(e) {
+    console.log('closed settings');
+  }
+);
